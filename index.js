1962186411b1073ec2ce56afa62b268e7ac8fcaf
shailesz <sailesh.karki@sagarmatha.edu.np>
2021-06-30 22:42:58 +0545

    kathmandu post ✔

--- index.js
+++ index.js
@@ -3,31 +3,59 @@ const axios = require("axios");
 const convert = require("xml-js");
 const admin = require("firebase-admin");
 const cheerio = require("cheerio");
-// import './serviceAccountKey.json'
-// import axios from "axios";
-// import convert from "xml-js";
-// import * as admin from 'firebase-admin';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-// var response = await axios.get("https://kathmandupost.com/rss");
-// var json = convert.xml2js(response.data, { compact: true, spaces: 4 });
-// console.log(json.rss.channel.item.length); 40
-
-// var response = await axios.get("https://nagariknews.nagariknetwork.com/feed");
-// var json = convert.xml2js(response.data, { compact: true, spaces: 4 });
-// console.log(json.rss.channel.item.length); 20
+const db = admin.firestore();
 
 // https://thehimalayantimes.com/rss big pain in the ass
 
+const getKathmanduPost = async () => {
+  //   "   &quot;
+  // '   &apos;
+  // <   &lt;
+  // >   &gt;
+  // &   &amp;
+  const docRef = db.collection("kathmandu post");
+
+  const { data } = await axios.get("https://kathmandupost.com/rss");
+  const { rss } = convert.xml2js(data.replace("&", "&amp;"), {
+    compact: true,
+    spaces: 4,
+  });
+
+  rss.channel.item.forEach(async (item) => {
+    const { title, link, description } = item;
+
+    const post = {
+      guid: link._text,
+      title: title._text,
+      description: description._text,
+      image: "dummy.jpg",
+      pubDate: Date.now(),
+      source: "Kathmandu Post",
+      section: link._text.split("/")[3],
+    };
+
+    try {
+      await docRef.add(post);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+};
+
+// TODO translate functionality in server
 const getNews = async () => {
-  var response = await axios.get("https://nagariknews.nagariknetwork.com/feed");
-  var json = convert.xml2js(response.data, { compact: true, spaces: 4 });
+  const response = await axios.get(
+    "https://nagariknews.nagariknetwork.com/feed"
+  );
+  const json = convert.xml2js(response.data, { compact: true, spaces: 4 });
 
   json.rss.channel.item.forEach(async (item) => {
-    var { title, link, pubDate, guid, description } = item;
+    const { title, link, pubDate, guid, description } = item;
 
     // Using cheerio to extract <a> tags
     const $ = cheerio.load(item["content:encoded"]._cdata);
@@ -42,26 +70,26 @@ const getNews = async () => {
       });
     });
 
+    // TODO make source static string
     var post = {
       guid: guid._text,
       title: title._text,
       description: description._text,
       image: images.length > 0 ? images[0].src : "dummy.jpg",
       pubDate: pubDate._text,
-      // genre
+      source: "Nagarik News",
+      section: guid._text.split("/")[3],
     };
+
+    const docRef = db.collection("nagarik");
 
     try {
       await docRef.add(post);
-      console.log(post);
     } catch (e) {
       console.log(e);
     }
   });
 };
 
-const db = admin.firestore();
-
-const docRef = db.collection("nagarik");
-
-getNews();
+// getNews();
+// getKathmanduPost();
