bec2e68293f96ad40ff6a7429b740f5e37cba42f
shailesz <sailesh.karki@sagarmatha.edu.np>
2021-07-18 08:54:30 +0545

    bug fix

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require("express");
 const main = require("./components/News");
 
+const path = require("path");
+const http = require("http");
+const express = require("express");
+
 const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log("server is up on port 3000");
 });
-
