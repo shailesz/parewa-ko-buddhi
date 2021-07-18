afb27f6efe0c2432fad5fa87864d1dc7beacb41d
shailesz <sailesh.karki@sagarmatha.edu.np>
2021-07-18 08:45:28 +0545

    server setup with express

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
 const main = require("./components/News");
 
-main();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
